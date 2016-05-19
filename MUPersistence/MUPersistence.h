f8e9d865f9f2afa78ae1c78ae01c10b9cebf60b3
muer <muer@Muer.local>
2016-05-19 16:18:59 +0800

    add header file

---
+++ MUPersistence/MUPersistence.h
@@ -0,0 +1,12 @@
+//
+//  MUPersistence.h
+//  MUPersistenceExample
+//
+//  Created by Muer on 16/5/19.
+//  Copyright © 2016年 Muer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "MUPersistent.h"
+#import "MUPersistentList.h"
